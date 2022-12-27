a816b9dc36ca40d9ce08e323b876b736418e28f8
cdoig4 <cdoig4@my.bcit.ca>
2022-12-27 15:11:55 -0800

    Create event listener for messages

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {
     token,
 } = require('./config.json');
 const ytdl = require('ytdl-core');
-
+const queue = new Map();
 const client = new Discord.Client();
 client.login(token);
 
@@ -17,3 +17,26 @@ client.once('reconnecting', () => {
 client.once('disconnect', () => {
     console.log('Disconnect!');
 });
+
+client.on('message', async message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const serverQueue = queue.get(message.guild.id);
+
+    if (message.content.startsWith(`${prefix}play`)) {
+        execute(message, serverQueue);
+        return;
+    } else if (message.content.startsWith(`${prefix}skip`)) {
+        skip(message, serverQueue);
+        return;
+    } else if (message.content.startsWith(`${prefix}stop`)) {
+        stop(message, serverQueue);
+        return;
+    } else {
+        message.channel.send('You need to enter a valid command!')
+    }
+});
+
+
+
