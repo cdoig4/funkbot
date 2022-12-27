d838612bdcaa43aeaf6141a2c70357f566991469
cdoig4 <cdoig4@my.bcit.ca>
2022-12-27 15:28:41 -0800

    Create execute function

--- index.js
+++ index.js
@@ -38,5 +38,57 @@ client.on('message', async message => {
     }
 });
 
+async function execute(message, serverQueue) {
+    const args = message.content.split(" ");
 
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+        return message.channel.send(
+            "You need to be in a voice channel to play music!"
+        );
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+        return message.channel.send(
+            "I need the permissions to join and speak in your voice channel!"
+        );
+    }
+    const songInfo = await ytdl.getInfo(args[1]);
+    const song = {
+        title: songInfo.title,
+        url: songInfo.video_url,
+    };
+    if (!serverQueue) {
+
+    } else {
+        serverQueue.songs.push(song);
+        console.log(serverQueue.songs);
+        return message.channel.send(`${song.title} has been added to the queue!`);
+    }
+    // Creating the contract for our queue
+    const queueContruct = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true,
+    };
+    // Setting the queue using our contract
+    queue.set(message.guild.id, queueContruct);
+    // Pushing the song to our songs array
+    queueContruct.songs.push(song);
+
+    try {
+        // Here we try to join the voicechat and save our connection into our object.
+        var connection = await voiceChannel.join();
+        queueContruct.connection = connection;
+        // Calling the play function to start a song
+        play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+        // Printing the error message if the bot fails to join the voicechat
+        console.log(err);
+        queue.delete(message.guild.id);
+        return message.channel.send(err);
+    }
+};
 
