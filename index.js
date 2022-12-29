bc7a97c8d5fbb42a0da617e2dda095b28bd9292b
cdoig4 <cdoig4@my.bcit.ca>
2022-12-29 09:19:50 -0800

    Add dependencies to file

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-require('dotenv').config();
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-})
-client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong");
-    }
-})
-client.login(process.env.DISCORD_TOKEN);
+// require('dotenv').config();
+// const Discord = require("discord.js");
+// const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}!`)
+// })
+// client.on("message", msg => {
+//     if (msg.content === "ping") {
+//         msg.reply("pong");
+//     }
+// })
+// client.login(process.env.DISCORD_TOKEN);
+
+const Discord = require(“discord.js”);
+const { prefix, token } = require(“./config.json”);
+const ytdl = require(“ytdl - core”);
+const client = new Discord.Client();
+const queue = new Map();
