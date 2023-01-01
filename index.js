7f78de7b80721c0bacf7bb802cc0e0546b1a74bb
cdoig4 <cdoig4@my.bcit.ca>
2022-12-31 16:31:41 -0800

    Implement command listener

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
 
 
 client.login(token);
